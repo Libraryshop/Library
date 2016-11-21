ab06c824d79788bbcec43391522115d596836459
Username-lph <120899698@qq.com>
2016-11-21 22:09:07 +0800

    config

--- config.js
+++ config.js
@@ -18,6 +18,30 @@ app.config(
 		  templateUrl:"bookshelf/searchFilter/searchFilter.html",	//搜寻过滤
 //		  controller:"loginCtrl"
 		  })
+		  .when("/comment",{
+		  	templateUrl:"newpages/article/comment/comment.html",
+		  })
+		  .when("/contents",{
+		  	templateUrl:"newpages/article/contents/contents.html",
+		  })
+		  .when("/copy",{
+		  	templateUrl:"newpages/article/copy/copy.html",
+		  })
+		  .when("/information",{
+		  	templateUrl:"newpages/article/information/information.html",
+		  })
+		  .when("/mode",{
+		  	templateUrl:"newpages/article/mode/mode.html",
+		  })
+		  .when("/reading",{
+		  	templateUrl:"newpages/article/reading/reading.html",
+		  })
+		  .when("/readList",{
+		  	templateUrl:"newpages/article/readList/readList.html",
+		  })
+		  .when("/share",{
+		  	templateUrl:"newpages/article/share/share.html",
+		  })
 		  
-		}]
-		 );
+		}]);
+		 
