1cb8b8edaaa0a5fd52faa4cd9830196b56841a8b
hugomin <363405513@qq.com>
2016-11-21 22:18:33 +0800

    h3

--- config.js
+++ config.js
@@ -18,6 +18,29 @@ app.config(
 		  templateUrl:"bookshelf/searchFilter/searchFilter.html",	//搜寻过滤
 //		  controller:"loginCtrl"
 		  })
-		  
+		  .when("/bc1",{											
+		  templateUrl:"bookcity/bc1/bc1.html",						//bc1
+//		  controller:"loginCtrl"
+		  })
+		   .when("/bc2",{
+		  templateUrl:"bookcity/bc2/bc2.html",						//bc2
+//		  controller:"loginCtrl"
+		  })
+		     .when("/bc3",{
+		  templateUrl:"bookcity/bc3/bc3.html",						//bc2
+//		  controller:"loginCtrl"
+		  })
+		       .when("/bc4",{
+		  templateUrl:"bookcity/bc4/bc4.html",						//bc2
+//		  controller:"loginCtrl"
+		  })
+		         .when("/bc5",{
+		  templateUrl:"bookcity/bc5/bc5.html",						//bc2
+//		  controller:"loginCtrl"
+		  })
+		           .when("/bc6",{
+		  templateUrl:"bookcity/bc6/bc6.html",						//bc2
+//		  controller:"loginCtrl"
+		  })
 		}]
 		 );
